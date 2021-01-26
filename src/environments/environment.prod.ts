1007aa6ffb0ac05858adcad1f90f2730b9336f81
julio <julio@paniagua.dev>
2021-01-26 01:45:30 +0100

    Fix prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA0gtcEzMqMBJYHkMZS7Bvf6GBcibgGLFo',
+    authDomain: 'mdc-europe-79a4e.firebaseapp.com',
+    databaseURL: 'https://mdc-europe-79a4e-default-rtdb.firebaseio.com',
+    projectId: 'mdc-europe-79a4e',
+    storageBucket: 'mdc-europe-79a4e.appspot.com',
+    messagingSenderId: '486856637340',
+    appId: '1:486856637340:web:9bcd4cbeebc413cd57e659',
+  },
 };
